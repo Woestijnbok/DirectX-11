c32ca36cd88b52ef748285815ecbdcc51399da67
Artuur Demeyer <Artuur.Demeyer@Outlook.com>
2024-11-02 17:18:58 +0100

    Redone file structure and changed some controls

---
+++ DirectX/Camera.cpp
@@ -0,0 +1,118 @@
+#include "pch.h"
+#include "Camera.h"
+#include <SDL_keyboard.h>
+#include <SDL_mouse.h>
+
+using namespace dae;
+
+Camera::Camera(const Vector3& origin, float fovDegrees, float ascpectRatio) :
+	m_FieldOfView{ tanf((fovDegrees * TO_RADIANS) / 2.0f) },
+	m_AspectRatio{ ascpectRatio },
+	m_Origin{ origin },
+	m_Forward{ Vector3::UnitZ },
+	m_Up{ Vector3::UnitY },
+	m_Right{ Vector3::UnitX },
+	m_TotalPitch{ 0.0f, },
+	m_TotalYaw{ 0.0f, },
+	m_ViewMatrix{},
+	m_ProjectionMatrix{},
+	m_NearPlane{ 1.0f },
+	m_FarPlane{ 1000.0f },
+	m_MovementSpeed{ 50.0f },
+	m_RotationSpeed{ 45.0f }
+{
+	CalculateProjectionMatrix();
+}
+
+void Camera::CalculateViewMatrix()
+{
+	m_Right = Vector3{ Vector3::Cross(Vector3::UnitY, m_Forward).Normalized() };
+	m_Up = Vector3{ Vector3::Cross(m_Forward, m_Right).Normalized() };
+	m_ViewMatrix = Matrix{ m_Right, m_Up, m_Forward, m_Origin }.Inverse();
+
+	//DirectX Implementation => https://learn.microsoft.com/en-us/windows/win32/direct3d9/d3dxmatrixlookatlh
+}
+
+void Camera::CalculateProjectionMatrix()
+{
+	m_ProjectionMatrix = Matrix
+	{
+		Vector4{ ((1.0f) / (m_AspectRatio * m_FieldOfView)), 0.0f, 0.0f, 0.0f },
+		Vector4{ 0.0f, ((1.0f) / (m_FieldOfView)), 0.0f, 0.0f},
+		Vector4{ 0.0f, 0.0f, ((m_FarPlane) / (m_FarPlane - m_NearPlane)), 1.0f },
+		Vector4{ 0.0f, 0.0f, -((m_FarPlane * m_NearPlane) / (m_FarPlane - m_NearPlane)), 0.0f }
+	};
+
+	//DirectX Implementation => https://learn.microsoft.com/en-us/windows/win32/direct3d9/d3dxmatrixperspectivefovlh
+}
+
+void Camera::Update(float deltaSeconds)
+{
+	//Camera Update Logic
+	float movementSpeed{ m_MovementSpeed };
+
+	//Keyboard Input
+	const uint8_t* pKeyboardState = SDL_GetKeyboardState(nullptr);
+
+	if (pKeyboardState[SDL_SCANCODE_LSHIFT])
+	{
+		movementSpeed *= 2.0f;
+	}
+	if (pKeyboardState[SDL_SCANCODE_W] || pKeyboardState[SDL_SCANCODE_UP])
+	{
+		m_Origin += m_Forward * movementSpeed * deltaSeconds;
+	}
+	if (pKeyboardState[SDL_SCANCODE_S] || pKeyboardState[SDL_SCANCODE_DOWN])
+	{
+		m_Origin -= m_Forward * movementSpeed * deltaSeconds;
+	}
+	if (pKeyboardState[SDL_SCANCODE_D] || pKeyboardState[SDL_SCANCODE_RIGHT])
+	{
+		m_Origin += m_Right * movementSpeed * deltaSeconds;
+	}
+	if (pKeyboardState[SDL_SCANCODE_A] || pKeyboardState[SDL_SCANCODE_LEFT])
+	{
+		m_Origin -= m_Right * movementSpeed * deltaSeconds;
+	}
+	if (pKeyboardState[SDL_SCANCODE_Q] || pKeyboardState[SDL_SCANCODE_RIGHT])
+	{
+		m_Origin += m_Up * movementSpeed * deltaSeconds;
+	}
+	if (pKeyboardState[SDL_SCANCODE_E] || pKeyboardState[SDL_SCANCODE_LEFT])
+	{
+		m_Origin -= m_Up * movementSpeed * deltaSeconds;
+	}
+
+	//Mouse Input
+	int mouseX{}, mouseY{};
+	const uint32_t mouseState = SDL_GetRelativeMouseState(&mouseX, &mouseY);
+
+	const bool RightMouseButtonDown{ bool(mouseState & SDL_BUTTON_RMASK) };
+
+	if (RightMouseButtonDown)
+	{
+		m_TotalYaw += float(mouseX) * m_RotationSpeed * deltaSeconds;
+		m_TotalPitch -= float(mouseY) * m_RotationSpeed * deltaSeconds;
+	}
+
+	Matrix finalRotation{ Matrix::CreateRotation(m_TotalPitch, m_TotalYaw, 0.0f) };
+	m_Forward = finalRotation.TransformVector(Vector3::UnitZ);
+	m_Forward.Normalize();
+
+	CalculateViewMatrix();
+}
+
+const Vector3& Camera::GetPosition() const
+{
+	return m_Origin;
+}
+
+const Matrix& Camera::GetViewMatrix() const
+{
+	return m_ViewMatrix;
+}
+
+const Matrix& Camera::GetProjectionMatrix() const
+{
+	return m_ProjectionMatrix;
+}
---
+++ DirectX/main.cpp
@@ -0,0 +1,88 @@
+#include "pch.h"
+
+#undef main
+#include "Renderer.h"
+
+using namespace dae;
+
+void ShutDown(SDL_Window* pWindow)
+{
+	SDL_DestroyWindow(pWindow);
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	//Unreferenced parameters
+	(void)argc;
+	(void)args;
+
+	//Create window + surfaces
+	SDL_Init(SDL_INIT_VIDEO);
+
+	const uint32_t width = 640;
+	const uint32_t height = 480;
+
+	SDL_Window* pWindow = SDL_CreateWindow(
+		"DirectX - Demeyer Artuur",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		width, height, 0);
+
+	if (!pWindow)
+		return 1;
+
+	//Initialize "framework"
+	const auto pTimer = new Timer();
+	const auto pRenderer = new Renderer(pWindow);
+
+	//Start loop
+	pTimer->Start();
+	float printTimer = 0.f;
+	bool isLooping = true;
+	while (isLooping)
+	{
+		//--------- Get input events ---------
+		SDL_Event e;
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+			case SDL_QUIT:
+				isLooping = false;
+				break;
+			case SDL_KEYUP:
+				//Test for a key
+				if (e.key.keysym.scancode == SDL_SCANCODE_1) pRenderer->SwitchSampleFilter();
+				else if (e.key.keysym.scancode == SDL_SCANCODE_R) pRenderer->ToggleRotate();
+				else if (e.key.keysym.scancode == SDL_SCANCODE_2) pRenderer->ToggleNormalMap();
+				else if (e.key.keysym.scancode == SDL_SCANCODE_3) pRenderer->ToggleFire();
+				break;
+			}
+		}
+
+		//--------- Update ---------
+		pRenderer->Update(pTimer->GetElapsed());
+
+		//--------- Render ---------
+		pRenderer->Render();
+
+		//--------- Timer ---------
+		pTimer->Update();
+		printTimer += pTimer->GetElapsed();
+		if (printTimer >= 1.f)
+		{
+			printTimer = 0.f;
+			std::cout << "dFPS: " << pTimer->GetdFPS() << std::endl;
+		}
+	}
+	pTimer->Stop();
+
+	//Shutdown "framework"
+	delete pRenderer;
+	delete pTimer;
+
+	SDL_DestroyWindow(pWindow);
+	SDL_Quit();
+	return 0;
+}
